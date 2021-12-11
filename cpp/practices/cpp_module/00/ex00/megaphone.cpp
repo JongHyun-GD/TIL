8a81c9f138d563837e3675ec889745907339a40f
Jonghyun Park <jhyun5717@gmail.com>
2021-12-11 14:32:12 +0900

    Fix megaphone echo uppercase

--- cpp/practices/cpp_module/00/ex00/megaphone.cpp
+++ cpp/practices/cpp_module/00/ex00/megaphone.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-void megaphone(int argc, const char **argv)
+void megaphone(int argc, char **argv)
 {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else {
 		for (int i = 1; i < argc; ++i) {
-			std::cout << argv[i];
+			std::string str(argv[i]);
+			for (size_t j = 0; j < str.length(); ++j)
+				std::cout << (char)std::toupper(str[j]);
 		}
 		std::cout << "\n";
 	}
