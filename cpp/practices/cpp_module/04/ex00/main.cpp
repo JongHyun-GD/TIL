73710aa302141f07813864533e730a12612a93ce
JongHyun-GD <58730856+JongHyun-GD@users.noreply.github.com>
2022-01-23 22:15:16 +0900

    Fix main.cpp

--- cpp/practices/cpp_module/04/ex00/main.cpp
+++ cpp/practices/cpp_module/04/ex00/main.cpp
@@ -4,14 +4,12 @@
 
 int main()
 {
-	//const Animal *meta = new Animal();
-	const Animal *j = new Dog();
-	const Animal *i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); // will output the cat sound!
-	j->makeSound();
-	//meta->makeSound();
+	const Animal *dog1 = new Dog();
+	const Animal *cat1 = new Cat();
+	std::cout << dog1->getType() << " " << std::endl;
+	std::cout << cat1->getType() << " " << std::endl;
+	cat1->makeSound(); // will output the cat sound!
+	dog1->makeSound();
 
 	// Wrong inheritance test
 	const WrongAnimal *wrong_i = new WrongCat();
